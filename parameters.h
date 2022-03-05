7987a941bc75e7f930826c6d19e644df328accf3
hide4096 <asouhide2002@gmail.com>
2022-03-05 16:55:00 +0900

    a

--- parameters.h
+++ parameters.h
@@ -49,8 +49,8 @@
 //走行パラメータ
 #define SEARCH_SPEED	(0.60)				//探索走行の速度	[m/s]
 #define SEARCH_ACCEL	(2.5)				//探索走行の加速度	[m/s^2]
-#define FAST_SPEED	(1.5)				//最短走行の速度	[m/s]
-#define FAST_ACCEL	(3.0)				//最短走行の加速度	[m/s^2]
+#define FAST_SPEED	(0.60)				//最短走行の速度	[m/s]
+#define FAST_ACCEL	(2.5)				//最短走行の加速度	[m/s^2]
 #define MIN_SPEED	(0.1)				//最低速度	[m/s]
 #define INT_BACK 5
 
