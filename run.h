2346be173402132ca1d0814092ed26be85d0bd87
hide4096 <asouhide2002@gmail.com>
2022-03-05 15:40:09 +0900

    master

---
+++ run.h
@@ -0,0 +1,5 @@
+void running(float len, float acc, float max_sp, float end_sp,t_bool mode,unsigned int limit_timer);
+void straight(float len, float acc, float max_sp, float end_sp);
+void straight_NC(float len, float acc, float max_sp, float end_sp);
+void back(float len);
+void turn(int deg, float ang_acc, float max_om, short dir);
